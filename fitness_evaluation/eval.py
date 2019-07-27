3e781bd907b9e61efef685999446430a2aef4d8a
Kelvin <kellar.base09@gmail.com>
2019-07-27 19:50:40 +0000

    Set up to test evaluate

--- fitness_evaluation/eval.py
+++ fitness_evaluation/eval.py
@@ -26,3 +26,16 @@ def evaluate(events_in_schedule, events_in_prev_schedule, Numberings):
     # perform final evaluation
     cost = final_evaluation(Numberings)
     return [cost, values]
+
+if __name__ == '__main__':
+    from constraints import \
+        pN0, pN1, pN2, \
+        N0, N1, N2, M_LIST
+    
+    events = [1,0,0, 1,0,0, 0,1,0, 0,1,0, 0,0,1, 0,0,0, 0,0,0]
+    prev_events = [0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 1,0,0, 1,0,0]
+
+    numberings = [N0, N1, N2]
+    prev_numberings = [pN0, pN1, pN2]
+
+    evaluate(events, prev_events, numberings, prev_numberings, M_LIST)
