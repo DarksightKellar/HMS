87f47d0295d28230df2fc2657a11e7d4ea27ef02
Kelvin <kellar.base09@gmail.com>
2019-07-27 19:51:02 +0000

    Better understanding, better code

--- fitness_evaluation/eval.py
+++ fitness_evaluation/eval.py
@@ -3,28 +3,56 @@ from final_evaluation import *
 from intermediate_evaluation import *
 
 
+def get_nth_event_numberings(n, events, numberings):
+    '''
+    For all numberings in  `numberings`, 
+    get the numbering of the `n`th event in `events`, in a list
+
+    Returns `None` if there is no `n`th event
+    '''
+    events_encountered = 0
+    event_index = 0
+    event_numberings = []
+
+    # Find index of event n
+    while events_encountered < n:
+        while events[event_index] is not 1:
+            event_index += 1
+
+            # if event_index reaches event list size, we've run outta events
+            if event_index == len(events):
+                return None
+
+        events_encountered += 1
+
+    for numbering in numberings:
+        event_numberings.append(numbering[event_index])
+
+    return event_numberings
+    
+
 # Every personal schedule is evaluated separately
-def evaluate(events_in_schedule, events_in_prev_schedule, Numberings):
+def evaluate(events_in_schedule, events_in_prev_schedule, numberings, prev_numberings, M_LIST):
     # initialise counter values
-    values = initialise(Numberings, events_in_prev_schedule)
+    values = initialise(prev_numberings, events_in_prev_schedule, M_LIST)
     cost = 0
 
-    # get numbering of first event in `events_in_schedule`
-    e = 0
-    event_numbering = events_in_schedule[e]
+    # get numberings of first event in `events_in_schedule`
+    event_n = 1
+    event_numberings = get_nth_event_numberings(event_n, events_in_schedule, numberings)
 
     end_of_solution = False
 
     while not end_of_solution:
         # update all numbering counters with intermediate evaluation
-        values = intermediate_evaluation(Numberings, event, values, last_nr)
+        values = intermediate_evaluation(numberings, event, values, last_nr)
 
         # get numbering of next event in planning period
-        e = e + 1
-        event_numbering = events_in_schedule[e]
+        event_n += 1
+        event_numberings = get_nth_event_numberings(event_n, events_in_schedule, numberings)
 
     # perform final evaluation
-    cost = final_evaluation(Numberings)
+    cost = final_evaluation(numberings)
     return [cost, values]
 
 if __name__ == '__main__':
