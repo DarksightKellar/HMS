a48872b3aaaf6850648895547215751d666a575d
Kelvin <kellar.base09@gmail.com>
2019-08-16 13:43:20 +0000

    Move test run to main.py, the beginning of finalising HSA

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # java -jar data/evaluate.jar -p data/test.xml -s data/test_solution.xml
-from ordering import ordering
+from instance import *
+from hsa import HarmonySearch
 
-print('running ordering()...')
-# ordering([], [])
+print('Setting up instance...')
+
+instance = Instance.create_test_instance()
+
+print('setting up Harmony Search...')
+hsa = HarmonySearch()
+hsa.setup(instance)
+print('done')
