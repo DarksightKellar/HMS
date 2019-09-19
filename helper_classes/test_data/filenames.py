618301fb3923155ac6ecfc31dc67de17d8259954
Kelvin <kellar.base09@gmail.com>
2019-09-19 13:44:35 +0000

    add other sprint_early instances to filenames

--- helper_classes/test_data/filenames.py
+++ helper_classes/test_data/filenames.py
@@ -1,5 +1,14 @@
 sprint01 = 'helper_classes/test_data/sprint_early/sprint01.xml'
+sprint02 = 'helper_classes/test_data/sprint_early/sprint02.xml'
+sprint03 = 'helper_classes/test_data/sprint_early/sprint03.xml'
+sprint04 = 'helper_classes/test_data/sprint_early/sprint04.xml'
+sprint05 = 'helper_classes/test_data/sprint_early/sprint05.xml'
+sprint06 = 'helper_classes/test_data/sprint_early/sprint06.xml'
+sprint07 = 'helper_classes/test_data/sprint_early/sprint07.xml'
+sprint08 = 'helper_classes/test_data/sprint_early/sprint08.xml'
+sprint09 = 'helper_classes/test_data/sprint_early/sprint09.xml'
+sprint10 = 'helper_classes/test_data/sprint_early/sprint10.xml'
 test = 'data/test.xml'
 
-INSTANCE_XML = sprint01
+INSTANCE_XML = sprint02
 SOLUTION_XML = 'data/solution.xml'
